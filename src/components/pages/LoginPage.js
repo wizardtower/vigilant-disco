e1713230af0ede9ea0068560706a9f58cfb1a221
wizardtower <erik.bremstedt@gmail.com>
2018-08-23 18:16:51 +0200

    lesson 2 done

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import LoginForm from '../forms/LoginForm'
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 
 class LoginPage extends React.Component {
 
-submit = (data) => {
-  console.log(data);
-};
+submit = (data) => this.props.login(data).then(() => this.props.history.pushh("/"));
 
   render(){
     return(
@@ -17,4 +18,11 @@ submit = (data) => {
   }
 }
 
-export default LoginPage;
+LoginPage.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }).isRequired,
+  login: PropTypes.func.isRequired
+};
+
+export default connect(null, { login })(LoginPage);
